40de316fb886843d9b31d4c1636884f491fcb3f3
Famsam OJ <famsamjunior@gmail.com>
2026-02-21 08:20:30 -0800

    Add the starting files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("mineBtn").onclick = function () {
+    window.location.href = "mine.html";
+};
+
+document.getElementById("homeBtn").onclick = function () {
+    window.location.href = "home.html";
+};
+
+alert ('JS is connected')
