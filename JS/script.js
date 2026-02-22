be6395a63467e46501675c173c1f5551dd6b99ef
Famsam OJ <famsamjunior@gmail.com>
2026-02-22 07:08:59 -0800

    Organize css and button configuration

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+document.getElementById("mineBtn").onclick = function () {
+    window.location.href = "mine.html";
+};
+
+document.getElementById("homeBtn").onclick = function () {
+    window.location.href = "home.html";
+};
+
+document.getElementById("shopBtn").onclick = function () {
+    window.location.href = "shop-tier1.html";
+};
+
+document.getElementById("pledgeBtn").onclick = function () {
+    window.location.href = "pledge.html";
+};
--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("mineBtn").onclick = function () {
-    window.location.href = "mine.html";
-};
-
-document.getElementById("homeBtn").onclick = function () {
-    window.location.href = "home.html";
-};
-
-alert ('JS is connected')
