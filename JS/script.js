7203dca962dfad1758ddc7a156e81d2254d3f778
Famsam OJ <famsamjunior@gmail.com>
2026-02-22 08:19:34 -0800

    button navigation

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,74 @@
-document.getElementById("mineBtn").onclick = function () {
+const mineBtn = document.getElementById("mineBtn");
+if (mineBtn) {
+  mineBtn.onclick = () => {
     window.location.href = "mine.html";
-};
+  };
+}
 
-document.getElementById("homeBtn").onclick = function () {
-    window.location.href = "home.html";
-};
-
-document.getElementById("shopBtn").onclick = function () {
+const shopBtn = document.getElementById("shopBtn");
+if (shopBtn) {
+  shopBtn.onclick = () => {
     window.location.href = "shop-tier1.html";
-};
+  };
+}
 
-document.getElementById("pledgeBtn").onclick = function () {
+const pledgeBtn = document.getElementById("pledgeBtn");
+if (pledgeBtn) {
+  pledgeBtn.onclick = () => {
     window.location.href = "pledge.html";
-};
+  };
+}
+
+const homeBtn = document.getElementById("homeBtn");
+if (homeBtn) {
+  homeBtn.onclick = () => {
+    window.location.href = "home.html";
+  };
+}
+
+const inviteBtn = document.getElementById("inviteBtn");
+if (inviteBtn) {
+  inviteBtn.onclick = () => {
+    window.location.href = "invite.html";
+  };
+}
+
+const minerBtn = document.getElementById("minerBtn");
+if (minerBtn) {
+  minerBtn.onclick = () => {
+    window.location.href = "miner-running.html";
+  };
+}
+
+const bonusBtn = document.getElementById("bonusBtn");
+if (bonusBtn) {
+  bonusBtn.onclick = () => {
+    window.location.href = "bonus.html";
+  };
+}
+
+const companyBtn = document.getElementById("companyBtn");
+if (companyBtn) {
+  companyBtn.onclick = () => {
+    window.location.href = "company.html";
+  };
+}
+
+const backBtn = document.getElementById("backBtn");
+if (backBtn) {
+  backBtn.onclick = () => history.back();
+}
+
+const tier1Btn = document.getElementById("tier1Btn");
+if (tier1Btn) {
+  tier1Btn.onclick = () => {
+    window.location.href = "shop-tier1.html";
+  };
+}
+
+const tier2Btn = document.getElementById("tier2Btn");
+if (tier2Btn) {
+  tier2Btn.onclick = () => {
+    window.location.href = "shop-tier2.html";
+  };
+}
